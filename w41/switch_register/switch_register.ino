ad2dd9fe6f4ac45940f2882cb1d78066d4ea2f3c
Theis <theistengs@gmail.com>
2021-11-11 12:16:45 +0100

    DIn mor fed ko Jan

--- w41/switch_register/switch_register.ino
+++ w41/switch_register/switch_register.ino
@@ -5,18 +5,22 @@
 #define clockPin 5
 //#define clearPin 6
 
+const byte interruptPinA = 3;
+const byte interruptPinB = 2;
+
 #define dirA 12
-#define spA 3
-#define brA 9
+#define spA 6
 #define dirB 13
 #define spB 11
-#define brB 8 
 
-#define A 2
-#define B 6
+#define A 8
+#define B 9
 #define C 10
 
 #define multiplexerIO A5
+
+volatile int numA = 0;
+volatile int numB = 0;
 
 bool readFromMulti(int chnl){
   int a = bitRead(chnl,0); //Take first bit from binary value of i channel.
@@ -55,35 +59,37 @@ class Button {
 };
 
 Button forwardButton(0);
-Button backwardButton(1);
+Button reverseButton(1);
 Button leftButton(2);
 Button rightButton(3);
 Button playButton(4);
 Button pauseButton(5);
 Button stopButton(6);
 
-LinkedList<String> commands;
+LinkedList<int> commands;
 String state = "input";
 int commandIndex = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
+  pinMode(multiplexerIO, INPUT_PULLUP);
   pinMode(A, OUTPUT);
   pinMode(B, OUTPUT);
   pinMode(C, OUTPUT);
-  pinMode(multiplexerIO, INPUT_PULLUP);
   
-  Serial.begin(9600);
   forwardButton.begin();
-  backwardButton.begin();
+  reverseButton.begin();
   leftButton.begin();
   rightButton.begin();
   playButton.begin();
   pauseButton.begin();
   stopButton.begin();
+
+  pinMode(interruptPinA, INPUT_PULLUP);
+  pinMode(interruptPinB, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPinA), incrementA, RISING);
+  attachInterrupt(digitalPinToInterrupt(interruptPinB), incrementB, RISING);
+  Serial.begin(9600);
 }
 
 
@@ -91,19 +97,19 @@ void loop() {
   // Input state
   if(state == "input"){
     if(forwardButton.isReleased()){
-      commands.add("forward");
+      commands.add(0);
       Serial.println("forward pressed");
     }
-    if(backwardButton.isReleased()){
-      commands.add("backward");
-      Serial.println("backward pressed");
+    if(reverseButton.isReleased()){
+      commands.add(1);
+      Serial.println("reverse pressed");
     }
     if(leftButton.isReleased()){
-      commands.add("left");
+      commands.add(2);
       Serial.println("left pressed");
     }
     if(rightButton.isReleased()){
-      commands.add("right");
+      commands.add(3);
       Serial.println("right pressed");
     }
     
@@ -140,16 +146,41 @@ void loop() {
     delay(50);
 }
 
-void doCommand(String command){
-  //Do command
-  Serial.println(command);
+void doCommand(int command){
+  switch(command){
+      case 0:
+        forward();
+        break;
+       case 1:
+        reverse();
+        break;
+       case 2:
+        left();
+        break;
+       case 3:
+        right();
+        break;
+    }
+}
 
-  //Execute for x seconds and listen for pause/stop while doing command
-  int starttime = millis();
-  int endtime = starttime;
-  
-  while ((endtime - starttime) <= 1000) // do this loop for up to 1000mS
-  {
+// General drive function with pause/stop listener
+void drive(int ticksA, boolean directionA, int ticksB, boolean directionB){
+  while(numA <= ticksA || numB <= ticksB){
+    if(numA < ticksA){
+      analogWrite(spA, 120);
+      digitalWrite(dirA, directionA);
+      
+    }else{
+      analogWrite(spA, 0);
+    }
+    if(numB < ticksB){
+      analogWrite(spB, 120);
+      digitalWrite(dirB, directionB);
+      
+    }else{
+      analogWrite(spB, 0);
+    }
+
     if(pauseButton.isReleased()){
         state = "pause";
     }
@@ -157,10 +188,50 @@ void doCommand(String command){
       state = "input";
       commands.clear();
     }
-    endtime = millis();
+    
   }
+  analogWrite(spA, 0);
+  analogWrite(spB, 0);
+  numA = 0;
+  numB = 0;
 }
 
+// Incrementers for tachometer
+void incrementA(){
+  numA += 1;
+}
+
+void incrementB(){
+  numB += 1;
+
+}
+
+// Directional drive functions
+void forward(){
+  Serial.println("forward");
+  drive(40, HIGH, 40, HIGH);
+  Serial.println("done");
+}
+
+void left(){
+  Serial.println("left");
+  drive(40, HIGH, 0, HIGH);
+  Serial.println("done");
+}
+
+void right(){
+  Serial.println("right");
+  drive(0, HIGH, 40, HIGH);
+  Serial.println("done");
+}
+
+void reverse(){
+  Serial.println("reverse");
+  drive(40, LOW, 40, LOW);
+  Serial.println("done");
+}
+
+// Write a byte to shift register
 void writeToRegister(byte data) {
   digitalWrite(latchPin, LOW);
   //digitalWrite(clearPin, HIGH);
--- w43/photointerrupter/photointerrupter.ino
+++ w43/photointerrupter/photointerrupter.ino
@@ -1,29 +1,95 @@
 const byte ledPin = 4;
-const byte interruptPin = 3;
+const byte interruptPinA = 3;
+const byte interruptPinB = 2;
 volatile byte state = LOW;
-volatile int num = 0;
+volatile int numa = 0;
+volatile int numb = 0;
+volatile bool AIsDriving = true;
+volatile bool BIsDriving = true;
+int list [4] = {0, 1, 0, 1};a
+// forward = 0
+// left = 1
+// right = 2
 
-#define dirA 13
-#define spA 11
+
+#define dirB 13
+#define spB 11
+#define dirA 12
+#define spA 6
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), increment, RISING);
+  pinMode(interruptPinA, INPUT_PULLUP);
+  pinMode(interruptPinB, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPinA), incrementA, RISING);
+  attachInterrupt(digitalPinToInterrupt(interruptPinB), incrementB, RISING);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  while(num < 20){
-    analogWrite(spA, 90);
-    digitalWrite(dirA, HIGH);
+  for(int command : list){
+    switch(command){
+      case 0:
+        forward();
+        break;
+       case 1:
+        left();
+        break;
+    }
   }
-  analogWrite(spA, 0);
-  
+  STOP();
+    
 }
 
-void increment(){
-  num += 1;
+void incrementA(){
+  numa += 1;
+}
+
+void incrementB(){
+  numb += 1;
+
+}
+// ||
+void forward(){
+  drive(40, 40);
+}
+
+void left(){
+  drive(40, 0);
+}
+
+void right(){
+  drive(0, 40);
+}
+
+// ||
+void drive(int ticksA, int ticksB){
+  while(numa <= ticksA || numb <= ticksB){
+    if(numa < ticksA){
+      analogWrite(spA, 120);
+      digitalWrite(dirA, HIGH);
+      
+    }else{
+      analogWrite(spA, 0);
+    }
+    if(numb < ticksB){
+      analogWrite(spB, 120);
+      digitalWrite(dirB, HIGH);
+      
+    }else{
+      analogWrite(spB, 0);
+    }
+    
+  }
+  analogWrite(spA, 0);
+  analogWrite(spB, 0);
+  numa = 0;
+  numb = 0;
+}
+
+void STOP(){
+  analogWrite(spA, 0);
+  analogWrite(spB, 0);
 }
